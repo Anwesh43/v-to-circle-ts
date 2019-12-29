165b4b7e9fc7fa3e497f8039a36b831231221e5e
Anwesh43 <anweshthecool0@gmail.com>
2019-12-29 16:41:08 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -7,3 +7,34 @@ const foreColor : string = "#311B92"
 const backColor : string = "#BDBDBD"
 const nodes : number = 5
 const lines : number = 2
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
