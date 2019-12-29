2d6847fa433121dfee1c0052678e8baea113423e
Anwesh43 <anweshthecool0@gmail.com>
2019-12-29 16:36:42 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,9 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.02
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const foreColor : string = "#311B92"
+const backColor : string = "#BDBDBD"
+const nodes : number = 5
+const lines : number = 2
